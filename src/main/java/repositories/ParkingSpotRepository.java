419832007f4de93c47a1102a0bb316ac49e80431
Luciano Ferreira <luciano-f-s-@hotmail.com>
2022-02-20 09:32:52 -0300

    models-respositories-services

---
+++ src/main/java/repositories/ParkingSpotRepository.java
@@ -0,0 +1,14 @@
+package repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import models.ParkingSpotModel;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID>{
+	
+
+}
